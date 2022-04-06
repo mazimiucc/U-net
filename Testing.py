c091cf78231ad291e5005b10ea552ab9d75c4e37
Mohammadreza Azimi <79572023+mazimiucc@users.noreply.github.com>
2022-04-06 11:30:43 +0100

    Add files via upload

---
+++ Testing.py
@@ -0,0 +1,112 @@
+import os
+from tensorflow import keras
+import numpy as np
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras import layers
+import PIL
+from PIL import Image
+from PIL import ImageOps
+import random
+
+
+input_dir = "images/"
+target_dir = "annotations/trimaps/"
+img_size = (160, 160)
+num_classes = 3
+batch_size = 32
+
+input_img_paths = sorted(
+    [
+        os.path.join(input_dir, fname)
+        for fname in os.listdir(input_dir)
+        if fname.endswith(".jpg")
+    ]
+)
+target_img_paths = sorted(
+    [
+        os.path.join(target_dir, fname)
+        for fname in os.listdir(target_dir)
+        if fname.endswith(".png") and not fname.startswith(".")
+    ]
+)
+
+print("Number of samples:", len(input_img_paths))
+
+for input_path, target_path in zip(input_img_paths[:10], target_img_paths[:10]):
+    print(input_path, "|", target_path)
+    
+filename=input_img_paths[9]
+im = Image.open(filename)
+
+img = PIL.ImageOps.autocontrast(load_img(target_img_paths[9]))
+img.show()
+
+
+class OxfordPets(keras.utils.Sequence):
+    """Helper to iterate over the data (as Numpy arrays)."""
+
+    def __init__(self, batch_size, img_size, input_img_paths, target_img_paths):
+        self.batch_size = batch_size
+        self.img_size = img_size
+        self.input_img_paths = input_img_paths
+        self.target_img_paths = target_img_paths
+
+    def __len__(self):
+        return len(self.target_img_paths) // self.batch_size
+
+    def __getitem__(self, idx):
+        """Returns tuple (input, target) correspond to batch #idx."""
+        i = idx * self.batch_size
+        batch_input_img_paths = self.input_img_paths[i : i + self.batch_size]
+        batch_target_img_paths = self.target_img_paths[i : i + self.batch_size]
+        x = np.zeros((self.batch_size,) + self.img_size + (3,), dtype="float32")
+        for j, path in enumerate(batch_input_img_paths):
+            img = load_img(path, target_size=self.img_size)
+            x[j] = img
+        y = np.zeros((self.batch_size,) + self.img_size + (1,), dtype="uint8")
+        for j, path in enumerate(batch_target_img_paths):
+            img = load_img(path, target_size=self.img_size, color_mode="grayscale")
+            y[j] = np.expand_dims(img, 2)
+            # Ground truth labels are 1, 2, 3. Subtract one to make them 0, 1, 2:
+            y[j] -= 1
+        return x, y
+        
+
+# Split our img paths into a training and a validation set
+val_samples = 1000
+random.Random(1337).shuffle(input_img_paths)
+random.Random(1337).shuffle(target_img_paths)
+train_input_img_paths = input_img_paths[:-val_samples]
+train_target_img_paths = target_img_paths[:-val_samples]
+val_input_img_paths = input_img_paths[-val_samples:]
+val_target_img_paths = target_img_paths[-val_samples:]
+
+# Instantiate data Sequences for each split
+train_gen = OxfordPets(
+    batch_size, img_size, train_input_img_paths, train_target_img_paths
+)
+val_gen = OxfordPets(batch_size, img_size, val_input_img_paths, val_target_img_paths)
+
+
+
+model = keras.models.load_model('oxford_segmentation.h5') 
+
+# Generate predictions for all images in the validation set
+
+val_gen = OxfordPets(batch_size, img_size, val_input_img_paths, val_target_img_paths)
+val_preds = model.predict(val_gen)
+
+
+def display_mask(i):
+    """Quick utility to display a model's prediction."""
+    mask = np.argmax(val_preds[i], axis=-1)
+    mask = np.expand_dims(mask, axis=-1)
+    img = PIL.ImageOps.autocontrast(keras.preprocessing.image.array_to_img(mask))
+    img.show()
+
+
+# Display results for validation image #10
+i = 11
+
+# Display mask predicted by our model
+display_mask(i)  # Note that the model only sees inputs at 150x150.
